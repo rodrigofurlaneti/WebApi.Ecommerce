345290af9d3bf4c7c1f36f82caede91fcd76a090
rodrigofurlaneti <rodrigofurlaneti31@hotmail.com>
2024-08-06 10:24:33 -0300

    Add ajuste service collection extensions e documentação.

---
+++ WebApi.Ecommerce/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,24 @@
+﻿using Domain.Ecommerce.Model;
+using Microsoft.Extensions.DependencyInjection;
+using WebApi.Ecommerce.Data.Interface;
+using WebApi.Ecommerce.Data.Repository;
+
+namespace WebApi.Ecommerce.Extensions
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddRepositories(this IServiceCollection services)
+        {
+            services.AddScoped<IAccessLogRepository, AccessLogRepository>();
+            services.AddScoped<IAuthenticationRepository, AuthenticationRepository>();
+            services.AddScoped<IContactUsRepository, ContactUsRepository>();
+            services.AddScoped<IGeolocationRepository, GeolocationRepository>();
+            services.AddScoped<IOrderRepository, OrderRepository>();
+            services.AddScoped<IOrderProductRepository, OrderProductRepository>();
+            services.AddScoped<IProductRepository, ProductRepository>();
+            services.AddScoped<IUsersRepository, UsersRepository>();
+
+            return services;
+        }
+    }
+}
